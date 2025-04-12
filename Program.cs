925f99f1d7b7c010316d31cd963d5e4ac88075ea
M.Israr <israrbaig557799@gmail.com>
2025-04-12 11:53:07 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿ using System;
+ namespace Object_args{
+  class Program{
+       static void Main( String [] args){
+        Car c1 = new Car("Honda","Red");
+        Car c2=Copy(c1);
+        System.Console.WriteLine($"The model of car is {c2.model} & the color of car is{ c2.color}");
+
+       }
+       public static Car Copy( Car c)
+       {
+        return new Car(c.model,c.color);
+       }
+  
+  }
+     class Car{
+        public String model;
+        public String color;
+          public Car( String model ,String color)
+          {
+            this.model=model;
+            this.color=color;
+          }
+     }
+
+ }
